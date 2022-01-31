b4118cdfde7b420e991aa0dcfc3c1c9601a7d589
omer <peynirciomerfaruk@gmail.com>
2022-01-31 10:35:37 +0100

    getObjBinom created

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //expand("(x+1)^2"),"x^2+2x+1")
 
 function expand(expr) {
+  let objBinom = getObjBinom(expr);
+
+  console.log("objBinom :", objBinom);
+}
+expand("(-5r+154)^203");
+
+function getObjBinom(expr) {
   let regExp = /\((\-*\d*)(\w)(\+|\-)(\d+)\)\^(\d+)+/;
   let decodedArrayOfExpression = expr.match(regExp);
   let factorOfA = decodedArrayOfExpression[1];
@@ -8,27 +15,12 @@ function expand(expr) {
   let typeOfCalculation = decodedArrayOfExpression[3];
   let b = decodedArrayOfExpression[4];
   let exponent = decodedArrayOfExpression[5];
-  console.log(
-    "factorOfA:",
-    factorOfA,
-    "letterOfA:",
-    letterOfA,
-    "typeOfCalculation:",
-    typeOfCalculation,
-    "b:",
-    b,
-    "exponent:",
-    exponent
-  );
-}
-expand("(-5r+154)^203");
-function AninCarpaniniBul(expr) {
-  let regExpAninCarpani = /\((\-?\d*)/g;
-  if (expr.match(regExpAninCarpani)[1] != "") {
-    let result = expr.match(regExpAninCarpani).join("").substring(1);
-    console.log(expr.match(regExpAninCarpani));
-    return result;
-  } else {
-    return 1;
-  }
+
+  return {
+    factorOfA: factorOfA,
+    letterOfA: letterOfA,
+    typeOfCalculation: typeOfCalculation,
+    b: b,
+    exponent: exponent,
+  };
 }
